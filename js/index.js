02af9d6c5f297b5f934bde617aaafd058024cb79
pixies <pixies@riseup.net>
2019-10-07 16:46:04 -0300

    crie uma conexao via XMLHTTPRequest

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-alert('ola mundo')
+function criaLink(cidade){
+    return "https://api.mapbox.com/geocoding/v5/mapbox.places/Guanambi.json?access_token=pk.eyJ1IjoiY2ZsYmVkdWNhdG9yIiwiYSI6ImNrMTZrYm1vNTA1dWEzaGxqN2tmMTZlazcifQ.XXsWkpgiguegb-C7WQpGBA"
+}
+
+var consulta = new XMLHttpRequest();
+var url = criaLink();
+consulta.open('GET', url, true); //0
+
+consulta.onreadystatechange = function(e) {
+    console.log(this.readyState );
+    if (this.readyState == 4){
+        console.log(JSON.parse(this.response));
+    }
+}
+
+
+consulta.send(); //2
+
+
